fd9edb75bad1b09a521c5f422d6d07f50fdc36a5
Suresh N <forsuresh@yandex.com>
2019-04-14 23:45:05 -0400

    Successful Yelp get request.  Code needs a lot of cleanup but the syntax to add "Bearer Authorization"

--- Business_Search/Business_Search/Controllers/MapController.swift
+++ Business_Search/Business_Search/Controllers/MapController.swift
@@ -19,9 +19,37 @@ class MapController: UITabBarController {
     }
     
     
+    struct categoriesStruct: Codable {
+        var alias: String
+        var title: String
+    }
+    
+    struct YelpStruct: Codable {
+        var categories: [categoriesStruct]
+        var businesses: [String]
+        var terms: [[String: String]]
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
+        
+        let url = URL(string: "https://api.yelp.com/v3/autocomplete?text=del&latitude=37.786882&longitude=-122.399972")!
+        var request = URLRequest(url: url)
+        request.setValue("Bearer \(API_Key)", forHTTPHeaderField: "Authorization")
+
+        
+        URLSession.shared.dataTask(with: request){ (data, resp, err) in
+            if let err = err {print("error:\n\(err)")}
+            guard let dataObject =  data else {print("There's no data"); return}
+
+            do {
+                let answer = try JSONDecoder().decode(YelpStruct.self, from: dataObject)
+                print(answer)
+            } catch {
+                print("failed: \n\(error)")
+            }
+        }.resume()
     }
 }
 
