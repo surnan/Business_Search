9c4d3f7958c51589f19352b9cc7703d27a705abc
Suresh N <forsuresh@yandex.com>
2019-04-14 23:00:56 -0400

    .gitIgnore setup

---
+++ Business_Search/Business_Search/Models/YelpClient.swift
@@ -0,0 +1,9 @@
+//
+//  YelpClient.swift
+//  Business_Search
+//
+//  Created by admin on 4/14/19.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import Foundation
---
+++ Business_Search/UNSYNC.swift
@@ -0,0 +1,14 @@
+//
+//  UNSYNC.swift
+//  Business_Search
+//
+//  Created by admin on 4/14/19.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import Foundation
+
+
+let Client_ID = "HAFtBvJVKZ_UpyrJli-oEg"
+
+let API_Key = "rt_E-vKLelAn5DU32KMCyUfEU8icw0FNwx67L9CBQCXXfwFQzNwLbNb0bUPpUXiS-MJofwkScs3ioWB6zVDQpdtNG6SzA4uc_aAL5BO9KMGZqu87SOVhA1elS66zXHYx"
