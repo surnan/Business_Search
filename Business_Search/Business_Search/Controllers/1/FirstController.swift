15821bae670a858bc9bf92e4c04b1b11870b376f
Suresh N <forsuresh@yandex.com>
2019-04-16 09:13:50 -0400

    Navigation Menu with Search Controller Setup

---
+++ Business_Search/Business_Search/Controllers/1/FirstController.swift
@@ -0,0 +1,45 @@
+//
+//  FirstController.swift
+//  Business_Search
+//
+//  Created by admin on 4/16/19.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import UIKit
+
+let search = UISearchController(searchResultsController: nil)
+
+
+class FirstController: UIViewController, UISearchResultsUpdating {
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        // print("")
+        // guard let text = searchController.searchBar.text else { return }
+        // print(text)
+    }
+    
+    func setupNavigationMenu(){
+        search.searchResultsUpdater = self
+        search.obscuresBackgroundDuringPresentation = false
+//        search.searchBar.placeholder = "Type something here to search"
+        search.searchBar.backgroundColor = UIColor.blue
+        search.searchBar.tintColor = UIColor.white //Cancel Button when you are typing
+        search.searchBar.barStyle = .black      // Black background with light content
+        search.searchBar.barTintColor = UIColor.orange  //Don't see it
+        navigationItem.searchController = search
+        navigationItem.title = "Business Search"
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Left", style: .done, target: self, action: #selector(sayHello))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Right", style: .done, target: self, action: #selector(sayHello))        
+    }
+    
+    
+    @objc func sayHello(){
+        print("Hello")
+    }
+    
+    override func viewDidLoad() {
+        view.backgroundColor = UIColor.white
+        setupNavigationMenu()
+    }
+}
