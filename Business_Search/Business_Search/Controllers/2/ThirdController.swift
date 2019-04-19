0c11e50acb9ca350ae3fb6089f957fbda1e26448
Suresh N <forsuresh@yandex.com>
2019-04-19 18:22:31 -0400

    3 ViewControllers created and added to Tab Controller 	* TabController is presented after tapping a row in ResultsController

---
+++ Business_Search/Business_Search/Controllers/2/ThirdController.swift
@@ -0,0 +1,13 @@
+//
+//  ThirdController.swift
+//  Business_Search
+//
+//  Created by admin on 4/19/19.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import UIKit
+
+class ThirdController: UIViewController {
+    
+}
