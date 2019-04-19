e87921dbf3226bb862df5a12c907f68829ced607
Suresh N <forsuresh@yandex.com>
2019-04-18 23:22:05 -0400

    Yelp API Error Enum created.  The response from Yelp can now be checked for error codes on Yelp's side. 	* Check is made within 'func taskForYelpGetRequest(...)' because the response isn't going through escape closure like data & error
    Network Error Enum moved into separate file created just for Error Enums.

---
+++ Business_Search/Business_Search/Models/Yelp API/Response/YelpAPIErrorResponse.swift
@@ -0,0 +1,21 @@
+//
+//  APIErrorCodes.swift
+//  Business_Search
+//
+//  Created by admin on 4/18/19.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import Foundation
+
+
+struct YelpAPIErrorResponse: Codable {
+    struct ErrorStruct: Codable {
+        var code: String
+        var description: String
+        var field: String
+        var instance: String
+    }
+
+    var error: [ErrorStruct]
+}
