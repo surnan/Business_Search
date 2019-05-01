5c60c3825116d4517a9d6429135f4d72a57bfe32
Suresh N <forsuresh@yandex.com>
2019-05-01 17:50:38 -0400

    refactored some of the setup code outside of viewdidload

--- Business_Search/Business_Search/Controllers/OpeningController.swift
+++ Business_Search/Business_Search/Controllers/OpeningController.swift
@@ -14,12 +14,104 @@ struct Candy {
     let name : String
 }
 
+/*
+ func numberOfSections(in tableView: UITableView) -> Int {
+ return 1
+ }
+ 
+ func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+ if isFiltering() {
+ return filteredCandies.count
+ }
+ return candies.count
+ }
+ 
+ func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+ let cell = tableView.dequeueReusableCell(withIdentifier: defaultCellID, for: indexPath) as! DefaultCell
+ let candy: Candy
+ if isFiltering() {
+ candy = filteredCandies[indexPath.row]
+ } else {
+ candy = candies[indexPath.row]
+ }
+ cell.textLabel!.text = candy.name
+ return cell
+ }
+ */
+
 //let defaultCellID = "defaultCellID"
-class OpeningController: UIViewController, NSFetchedResultsControllerDelegate, UISearchControllerDelegate, UISearchBarDelegate  {
+class OpeningController: UIViewController, NSFetchedResultsControllerDelegate, UISearchControllerDelegate, UISearchBarDelegate {
     
     var dataController: DataController!  //MARK: Injected
-    var tableView = UITableView()
-    let searchController = UISearchController(searchResultsController: nil) //Going to use same View to display results
+//    var tableView = UITableView()
+    
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(DefaultCell.self, forCellReuseIdentifier: defaultCellID)
+        return tableView
+    }()
+    
+    
+    
+    
+    
+    
+    var myFetchController: NSFetchedResultsController<Location>!
+    var myCategories = [[Category]]()
+    var myBusinesses = [Business]()
+    var myLocations = [Location]()
+    var currentLocation: Location!
+    var doesLocationExist = false
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(tableView)
+        let safe = view.safeAreaLayoutGuide
+        tableView.anchor(top: safe.topAnchor, leading: safe.leadingAnchor, trailing: safe.trailingAnchor, bottom: safe.bottomAnchor)
+        setupNavigationMenu()
+    }
+    
+    
+    lazy var searchController: UISearchController = {
+        let searchController = UISearchController(searchResultsController: nil) //Going to use same View to display results
+        searchController.searchBar.scopeButtonTitles = ["All", "Chocolate", "Hard", "Other"]
+        searchController.searchBar.delegate = self
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search Candies"
+//        searchController.
+        return searchController
+    }()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    func setupNavigationMenu(){
+        let logo = UIImage(imageLiteralResourceName: "Inline-Logo")
+        let imageView = UIImageView(image: logo)
+        imageView.contentMode = .scaleAspectFit
+        self.navigationItem.titleView = imageView
+        navigationItem.searchController = searchController
+    }
+    
     
     var filteredCandies = [Candy]()
     var candies: [Candy] = [
@@ -47,34 +139,6 @@ class OpeningController: UIViewController, NSFetchedResultsControllerDelegate, U
         Candy(category:"Hard", name:"Toffee Apple")
     ]
     
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        searchController.searchBar.scopeButtonTitles = ["All", "Chocolate", "Hard", "Other"]
-        searchController.searchBar.delegate = self
-        searchController.searchResultsUpdater = self
-        searchController.obscuresBackgroundDuringPresentation = false
-        searchController.searchBar.placeholder = "Search Candies"
-        navigationItem.searchController = searchController
-        tableView.delegate = self
-        tableView.dataSource = self
-        tableView.register(DefaultCell.self, forCellReuseIdentifier: defaultCellID)
-        
-
-        [tableView].forEach{
-            $0.translatesAutoresizingMaskIntoConstraints = false
-            view.addSubview($0)
-        }
-
-        let safe = view.safeAreaLayoutGuide
-        
-        tableView.anchor(top: safe.topAnchor, leading: safe.leadingAnchor, trailing: safe.trailingAnchor, bottom: safe.bottomAnchor)
-        let logo = UIImage(imageLiteralResourceName: "Inline-Logo")
-
-        let imageView = UIImageView(image: logo)
-        imageView.contentMode = .scaleAspectFit
-        self.navigationItem.titleView = imageView
-    }
 }
 
 
