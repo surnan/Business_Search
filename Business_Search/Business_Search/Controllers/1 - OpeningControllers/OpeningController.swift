49092d21bb3cb95bfc67d760996aa7fdada6a512
Suresh N <forsuresh@yandex.com>
2019-05-04 10:22:38 -0400

    Implemented BarButtonItems on Opening Controller that give the following functionality: 	* Delete All Locations from CoreData 	* Re-Download Location from CoreData 	* Give current Category & Business counts
    Deleted lots of unnecessary files

--- Business_Search/Business_Search/Controllers/1 - OpeningControllers/OpeningController.swift
+++ Business_Search/Business_Search/Controllers/1 - OpeningControllers/OpeningController.swift
@@ -21,6 +21,8 @@ class OpeningController: UIViewController, NSFetchedResultsControllerDelegate, U
     var dataController: DataController!  //MARK: Injected
     var currentLocation: Location!
     var doesLocationExist = false
+    var urlsQueue = [YelpGetNearbyBusinessStruct]() //enumeration loop for semaphores
+    var currentLocationID: NSManagedObjectID?
     
     var searchGroupIndex = 0 //Only accessed directly in 'func selectedScopeButtonIndexDidChange'
     var tableViewArrayType: Int {
@@ -206,12 +208,14 @@ class OpeningController: UIViewController, NSFetchedResultsControllerDelegate, U
     }
 
     @objc func handleDownloadBusinesses(){
+
         deleteAll()
         ////////////////////////////////////////////////
         self.fetchBusinessController = nil
         self.fetchCategoriesController = nil
         ////////////////////////////////////////////////
-//        _ = YelpClient.getNearbyBusinesses(latitude: latitude, longitude: longitude, completion: handleLoadBusinesses(inputData:result:))
+
+        _ = YelpClient.getNearbyBusinesses(latitude: latitude, longitude: longitude, completion: handleLoadBusinesses(inputData:result:))
         
     }
     
@@ -240,21 +244,120 @@ class OpeningController: UIViewController, NSFetchedResultsControllerDelegate, U
             let fetch = NSFetchRequest<NSFetchRequestResult>(entityName: "Location")
             let deleteRequest = NSBatchDeleteRequest(fetchRequest: fetch)
             
-            ////////////////////////////////////////////////
-            self.fetchBusinessController = nil
-            self.fetchCategoriesController = nil
-            ////////////////////////////////////////////////
-            
             do {
                 _  = try context.execute(deleteRequest) as! NSBatchDeleteResult
+                
+                self.fetchBusinessController = nil
+                self.fetchCategoriesController = nil
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
                 //  context.reset()
             } catch {
                 print("Error deleting All \(error)")
             }
         }
     }
-    
-    
+}
 
+//po String(data: data, format: .utf8)
+extension OpeningController {
+    func createLocation(data: YelpBusinessResponse){
+        //Save Location Entity and Business Entities for the same API Call
+        let backgroundContext = dataController.backGroundContext!
+        backgroundContext.performAndWait {
+            //Synchronous to make sure //1 occurs before //2
+            let newLocation = Location(context: backgroundContext)
+            newLocation.latitude = data.region.center.latitude
+            newLocation.longitude = data.region.center.longitude
+            newLocation.totalBusinesses = Int32(data.total)
+            newLocation.radius = Int32(radius)  //AppDelegate
+            recordCountAtLocation = data.total
+            do {
+                try backgroundContext.save()    //1
+                self.currentLocationID = newLocation.objectID
+                newLocation.saveBusinessesAndCategories(yelpData: data, context: backgroundContext) //2
+                self.buildURLsQueueForDownloadingBusinesses(total: data.total)    //Because background context, best way to time save happens first
+            } catch {
+                print("Error saving func addLocation() --\n\(error)")
+            }
+        }
+    }
+    
+    func handleLoadBusinesses(inputData: YelpGetNearbyBusinessStruct?, result: Result<YelpBusinessResponse, NetworkError>){
+        switch result {
+        case .failure(let error):
+            if error == NetworkError.needToRetry || error == NetworkError.tooManyRequestsPerSecond {
+                print("handleLoadBusiness --> Retry -> \(error) ... inputData = \(String(describing: inputData))")
+            } else {
+                print("Error that is not 'needToRetry' --> error = \(error)")
+            }
+        case .success(let data):
+            if !doesLocationExist {
+                createLocation(data: data)
+                doesLocationExist = true
+            } else {
+                let filterIndex = urlsQueue.firstIndex { (element) -> Bool in
+                    guard let inputData = inputData else {return false}
+                    return element.offset == inputData.offset
+                }
+                if let indexToDelete = filterIndex {
+                    urlsQueue.remove(at: indexToDelete)
+                }
+                
+                dataController.persistentContainer.performBackgroundTask {[unowned self] (context) in
+                    //Giving Core Data the chance to perform multiple instances in parallel
+                    let currentLocation = context.object(with: self.currentLocationID!) as! Location
+                    currentLocation.saveBusinessesAndCategories(yelpData: data, context: context)
+                }
+            }
+        }
+    }
+    
+    func buildURLsQueueForDownloadingBusinesses(total: Int){
+        for index in stride(from: limit, to: recordCountAtLocation, by: limit){
+            urlsQueue.append(YelpGetNearbyBusinessStruct(latitude: latitude, longitude: longitude, offset: index))
+        }
+        downloadYelpBusinesses()
+    }
+    
+    func downloadYelpBusinesses(){
+        if urlsQueue.isEmpty {return}
+        let semaphore = DispatchSemaphore(value: 4)
+        let dispatchGroup = DispatchGroup()
+        
+        for (index, element) in urlsQueue.enumerated(){
+            dispatchGroup.enter()
+            semaphore.wait()
+            _ = YelpClient.getNearbyBusinesses(latitude: latitude, longitude: longitude, offset: element.offset ,completion: { [weak self] (yelpDataStruct, result) in
+                defer {
+                    semaphore.signal()
+                    dispatchGroup.leave()
+                }
+                self?.handleLoadBusinesses(inputData: yelpDataStruct, result: result)
+            })
+        }
+        dispatchGroup.notify(queue: .main) {[weak self] in
+            self?.runDownloadAgain()
+        }
+    }
+    
+    func runDownloadAgain(){
+        print("\nTimer fired!\nurlsQueue ------> \(self.urlsQueue)")
+
+        
+        self.fetchBusinessController = nil
+        self.fetchCategoriesController = nil
+        tableView.reloadData()
+        
+        print("fetchBusiness.FetchedObject.count - ", fetchBusinessController?.fetchedObjects?.count ?? -999)
+        print("fetchCategoryArray.count - ", fetchCategoryArray?.count ?? -999)
+        
+        
+        let timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: false) { [weak self] timer in
+            self?.downloadYelpBusinesses()
+        }
+        timer.fire()
+    }
 }
 
