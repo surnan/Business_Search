bd24c6646c5cde6034f7a9b9317fa087da033a1a
Suresh N <forsuresh@yandex.com>
2019-05-05 18:14:43 -0400

    Added Functionality 'Search By Address' from the main screen. 	* User enters location in text field, and coordinate is auto-generated.  This info is passed onto the VC that it pushes onto. 		* For now: OpenController()

---
+++ Business_Search/Business_Search/Views/NSAttributedStringKeys.swift
@@ -0,0 +1,35 @@
+//
+//  NSAttributedStringKeys.swift
+//  _OnTheMap
+//
+//  Created by admin on 2/12/19.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import UIKit
+
+let grey25textAttributes: [NSAttributedString.Key: Any] = [
+    NSAttributedString.Key.foregroundColor : UIColor.gray,
+    NSAttributedString.Key.font: UIFont(name: "Georgia", size: 25) as Any
+]
+
+let black25textAttributes: [NSAttributedString.Key: Any] = [
+    NSAttributedString.Key.foregroundColor : UIColor.black,
+    NSAttributedString.Key.font: UIFont(name: "Georgia", size: 25) as Any
+]
+
+let white25textAttributes: [NSAttributedString.Key: Any] = [
+    NSAttributedString.Key.foregroundColor : UIColor.white,
+    NSAttributedString.Key.font: UIFont(name: "Georgia", size: 25) as Any
+]
+
+let steelBlue4_25 : [NSAttributedString.Key: Any] = [
+    NSAttributedString.Key.foregroundColor : UIColor.steelBlue4,
+    NSAttributedString.Key.font: UIFont(name: "Georgia", size: 25) as Any
+]
+
+let orange_25 : [NSAttributedString.Key: Any] = [
+    NSAttributedString.Key.foregroundColor : UIColor.orange,
+    NSAttributedString.Key.font: UIFont(name: "Georgia", size: 25) as Any
+]
+
