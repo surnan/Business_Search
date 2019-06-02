d6007449199d23a58bfdfa70750d5fa2b37d3904
Suresh N <forsuresh@yandex.com>
2019-06-02 10:51:53 -0400

    Added ViewController extensions for Alert Controllers

--- Business_Search/Business_Search/Controllers/Settings Controller/SettingsController.swift
+++ Business_Search/Business_Search/Controllers/Settings Controller/SettingsController.swift
@@ -230,3 +230,18 @@ class SettingsController: UIViewController, NSFetchedResultsControllerDelegate {
         UserDefaults.standard.set(radius, forKey: AppConstants.radius.rawValue)
     }
 }
+
+extension UIViewController {
+    func showOKAlertController(title: String, message: String){
+        let myAlertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        myAlertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(myAlertController, animated: true)
+    }
+    
+    func showOKCancelAlertController(title: String, message: String, okFunction: ((UIAlertAction) -> Void)?) {
+        let myAlertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        myAlertController.addAction(UIAlertAction(title: "OK", style: .default, handler: okFunction))
+        myAlertController.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
+        present(myAlertController, animated: true)
+    }
+}
