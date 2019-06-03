4900988e23080c13bf1a4aaa8198ee1ce56f9032
Suresh N <forsuresh@yandex.com>
2019-06-03 18:25:25 -0400

    added class ManagedObjectCloner(Entity)-> Entity 	* that duplicates Core Data Entity

---
+++ Business_Search/Business_Search/Models/Core Data/ManagedObjectCloner.swift
@@ -0,0 +1,42 @@
+//
+//  ManagedObjectCloner.swift
+//  Business_Search
+//
+//  Created by admin on 6/3/19.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ManagedObjectCloner: NSObject {
+    
+    static func cloneObject(source :NSManagedObject, context :NSManagedObjectContext) -> NSManagedObject{
+        let entityName = source.entity.name
+        let cloned = NSEntityDescription.insertNewObject(forEntityName: entityName!, into: context)
+        
+        let attributes = NSEntityDescription.entity(forEntityName: entityName!, in: context)?.attributesByName
+        
+        for (key,_) in attributes! {
+            cloned.setValue(source.value(forKey: key), forKey: key)
+        }
+        
+        let relationships = NSEntityDescription.entity(forEntityName: entityName!, in: context)?.relationshipsByName
+        for (key,_) in relationships! {
+            let sourceSet = source.mutableSetValue(forKey: key)
+            let clonedSet = cloned.mutableSetValue(forKey: key)
+            let e = sourceSet.objectEnumerator()
+            
+            var relatedObj = e.nextObject() as? NSManagedObject
+            
+            while ((relatedObj) != nil) {
+                let clonedRelatedObject = ManagedObjectCloner.cloneObject(source: relatedObj!, context: context)
+                clonedSet.add(clonedRelatedObject)
+                relatedObj = e.nextObject() as? NSManagedObject
+            }
+        }
+        
+        return cloned
+    }
+    
+}
