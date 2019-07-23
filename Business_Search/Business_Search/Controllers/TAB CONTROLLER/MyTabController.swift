a96659b65332f5733f2f8151c81994722d39b34e
Suresh N <forsuresh@yandex.com>
2019-07-23 16:33:21 -0400

    code cleanup

--- Business_Search/Business_Search/Controllers/TAB CONTROLLER/MyTabController.swift
+++ Business_Search/Business_Search/Controllers/TAB CONTROLLER/MyTabController.swift
@@ -12,25 +12,11 @@ class MyTabController: UITabBarController {
     //MARK:- Injections START
     var category: String!
     var categoryName: String!  {didSet {navigationItem.title = categoryName}}
-    var businesses = [Business]()   {
-        didSet {
-            firstController.businesses = businesses     //Populate GroupsController.tableView
-            secondController.businesses = businesses    //populate MapsController.mapView
-        }
-    }
-    //MARK:- Injections END
-    
-    let firstController = GroupsController()
-    let secondController = MapController()
+    var businesses = [Business]()
+
     
     override func viewDidLoad() {
-//        view.backgroundColor = UIColor.clear
-//        navigationItem.title = categoryName
-//        firstController.tabBarItem =  UITabBarItem(title: "LIST", image: #imageLiteral(resourceName: "menu100B.png"), selectedImage: #imageLiteral(resourceName: "menu100B.png"))
-//        secondController.tabBarItem = UITabBarItem(title: "MAP", image: #imageLiteral(resourceName: "map100") , selectedImage: #imageLiteral(resourceName: "map100"))
-//        let tabBarList = [firstController, secondController]
-//        viewControllers = tabBarList
-//        tabBar.isTranslucent = false
+        super.viewDidLoad()
     }
 }
 
