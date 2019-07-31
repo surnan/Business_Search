ef98bfa0c07647f481942f708f5fa7fec789b6fb
Suresh N <forsuresh@yandex.com>
2019-07-31 13:12:58 -0400

    swipe action added to OpenController.  However, it's only GUI.  No functionality added.  Need to setup Favorites ViewModel and Favorites CoreData Fetch

---
+++ Business_Search/Business_Search/_Break it down/___/Open/TableView Delegate/Open_Delegate+LeadingSwype.swift
@@ -0,0 +1,11 @@
+//
+//  Open_Delegate+LeadingSwype.swift
+//  Business_Search
+//
+//  Created by admin on 7/31/19.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import UIKit
+
+
--- Business_Search/Business_Search/_Break it down/___/Open/TableView Delegate/Open_Delegate+RowAction.swift
+++ Business_Search/Business_Search/_Break it down/___/Open/TableView Delegate/Open_Delegate+RowAction.swift
@@ -11,19 +11,21 @@ import UIKit
 extension Open_Delegate {
     //MARK:- ROW RIGHT-SIDE ACTIONS
     func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
-        let actionBusiness = UITableViewRowAction(style: .normal, title: "SHARE") {[unowned self] (action, indexPath) in
+        //
+        let actionForBusiness = UITableViewRowAction(style: .normal, title: "SHARE") {[unowned self] (action, indexPath) in
             guard let currentBusiness = self.source.businessViewModel.fetchBusinessController?.object(at: indexPath) else {return}
             self.shareBusiness(business: currentBusiness)   //3
         }
-        actionBusiness.backgroundColor = .darkBlue
-        let actionCategory = UITableViewRowAction(style: .normal, title: "RANDOM") {[unowned self] (action, indexPath) in
+        //
+        let actionForCategory = UITableViewRowAction(style: .normal, title: "RANDOM") {[unowned self] (action, indexPath) in
             let currentCategory = self.getCategoryName(at: indexPath.row)      //1
             let items           = self.getBusinessesFromCategoryName(category: currentCategory)
             self.showRandomBusiness(businesses: items)  //2
         }
-        actionBusiness.backgroundColor = .red
-        actionCategory.backgroundColor = .blue
-        return source.tableArrayType == TableIndex.category.rawValue ? [actionCategory] : [actionBusiness]
+        //
+        actionForBusiness.backgroundColor = .red
+        actionForCategory.backgroundColor = .blue
+        return source.tableArrayType == TableIndex.category.rawValue ? [actionForCategory] : [actionForBusiness]
     }
     
     func getCategoryName(at index: Int) -> String {     //1
@@ -45,5 +47,28 @@ extension Open_Delegate {
         guard let temp = business.url else {return}
         let items: [Any] = ["\(prependText) \(temp)"]
         coordinator?.shareItems(items: items)
+    }
+}
+
+extension Open_Delegate {
+    func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        //if delegate.getModel.tableViewArrayType == TableIndex.category.rawValue {return nil}
+        //let currentBusiness = dataDelegate.getBusiness(at: indexPath)
+        if source.tableArrayType == TableIndex.category.rawValue {return nil}
+        guard let currentBusiness = source.businessViewModel.fetchBusinessController?.object(at: indexPath) else {return nil}
+        let action = UIContextualAction(style: .normal, title: "Favorite") { [weak self] (action, view, myBool) in
+            //guard let self  = self, let dd = self.dataDelegate, let delegate = self.delegate else {return}
+            guard let self = self else {return}
+            //            let isFavorite  = delegate.updateBusinessIsFavorite(business: currentBusiness)
+            //            isFavorite ? delegate.createFavorite(business: currentBusiness)
+            //                :delegate.deleteFavorite(business: currentBusiness)
+            //            dd.resetBusinessController()
+            //            delegate.reloadData()
+            myBool(true)                                //Dismiss the leading swipe from UI
+        }
+        action.image            = currentBusiness.isFavorite    ?  #imageLiteral(resourceName: "cancel") : #imageLiteral(resourceName: "Favorite")
+        action.backgroundColor  =  currentBusiness.isFavorite   ? .lightSteelBlue1 : .orange
+        let configuration       = UISwipeActionsConfiguration(actions: [action])
+        return configuration
     }
 }
