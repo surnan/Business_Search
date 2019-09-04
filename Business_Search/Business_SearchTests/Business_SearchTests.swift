e52861db3fafab15d96ac359996e8b971e0a70fb
Suresh N <forsuresh@yandex.com>
2019-09-04 18:36:30 -0400

    Unit-Test-Begin-setting-up-Category

--- Business_Search/Business_SearchTests/Business_SearchTests.swift
+++ Business_Search/Business_SearchTests/Business_SearchTests.swift
@@ -22,14 +22,18 @@ class Business_SearchTests: XCTestCase {
         return dataController.viewContext
     }
     
-    var myLocation              = LocationStruct()
-    let categoryStruct1         = CategoryStruct(alias: "pizza", title: "Pizza")
-    let categoryStruct2         = CategoryStruct(alias: "bakery", title: "Bakery")
-    var businessNotFavorite     = BusinessStruct(name: "Carve - not favorite", displayAddress: "760 8th Ave")
-    var businessIsFavorite      = BusinessStruct(name: "Tonka - is favorite", displayAddress: "140 Broadway", isFavorite: true)
+    var myLocation      = LocationStruct()
+    let categoryStruct1 = CategoryStruct(alias: "pizza", title: "Pizza")
+    let categoryStruct2 = CategoryStruct(alias: "bakery", title: "Bakery")
+    var myBusiness      = BusinessStruct(name: "Tonka - is favorite", displayAddress: "140 Broadway", isFavorite: true)
     
-    lazy var newLocation = Location(context: context)
-    lazy var newBusiness = Business(context: context)
+    lazy var newLocation    = Location(context: context)
+    lazy var newBusiness    = Business(context: context)
+    
+    
+    lazy var newCategory1   = Category(context: context)
+    lazy var newCategory2   = Category(context: context)
+    lazy var newCategories  = [newCategory1, newCategory2]
     
     let index0      = IndexPath(row: 0, section: 0)
     let index1      = IndexPath(row: 1, section: 0)
@@ -61,7 +65,28 @@ class Business_SearchTests: XCTestCase {
         XCTAssertNotEqual(cellModel3.getOriginalColor, offWhite)
     }
     
-    
+    func testCategoryCellViewModel(){
+        let cellModel0  = BusinessCellViewModel(business: newBusiness, colorIndex: index0)
+        let cellModel1  = BusinessCellViewModel(business: newBusiness, colorIndex: index1)
+        let cellModel2  = BusinessCellViewModel(business: newBusiness, colorIndex: index2)
+        let cellModel3  = BusinessCellViewModel(business: newBusiness, colorIndex: index3)
+        
+        //Label Text Check
+        if let name = newBusiness.name, let displayAddress = newBusiness.displayAddress {
+            let labelString = name + "\n" + displayAddress
+            XCTAssertEqual(cellModel0.getMyLabelAttributedString.string, labelString)
+        } else {
+            XCTAssertEqual(true, false)
+        }
+        
+        //BackGround Color Check odd numbers should be white
+        XCTAssertEqual(cellModel0.getOriginalColor, offWhite)
+        XCTAssertEqual(cellModel2.getOriginalColor, offWhite)
+        
+        //BackGround Color Check odd numbers should be not be white
+        XCTAssertNotEqual(cellModel1.getOriginalColor, offWhite)
+        XCTAssertNotEqual(cellModel3.getOriginalColor, offWhite)
+    }
     
     
     override func setUp() {
@@ -121,30 +146,21 @@ extension Business_SearchTests {
     }
     
     func addBusiness(id: NSManagedObjectID?){
-        let businessArray = [businessNotFavorite, businessIsFavorite]
         guard let id = id else {return}
         let parent = context.object(with: id) as! Location
+        
         newBusiness.parentLocation = parent
-        
-        //newBusiness.name = "tempName"
-        newBusiness.name = businessIsFavorite.name
-        
-        //newBusiness.alias = "tempAlias"
-        newBusiness.alias = businessIsFavorite.alias
-        
-        //newBusiness.id = "tempID"
-        newBusiness.id = businessIsFavorite.id
-        
-        //newBusiness.displayAddress = "140 tempAvenue"
-        newBusiness.displayAddress = businessIsFavorite.displayAddress
-        
+        newBusiness.name = myBusiness.name
+        newBusiness.alias = myBusiness.alias
+        newBusiness.id = myBusiness.id
+        newBusiness.displayAddress = myBusiness.displayAddress
         newBusiness.isDelivery = true
         newBusiness.isFavorite = false
         newBusiness.isPickup = true
         newBusiness.latitude = 1111
         newBusiness.longitude = 1111
-        
-        
+
+
         do {
             try context.save()
             addCategories(id: newBusiness.objectID)
@@ -158,12 +174,23 @@ extension Business_SearchTests {
         guard let id = id else {return}
         let parent = context.object(with: id) as! Business
         let categories = [categoryStruct1, categoryStruct2]
+        
         categories.forEach { (element) in
             let newCategory = Category(context: context)
             newCategory.title = element.title
             newCategory.alias = element.alias
             newCategory.business = parent
         }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         do {
             try context.save()
         } catch {
