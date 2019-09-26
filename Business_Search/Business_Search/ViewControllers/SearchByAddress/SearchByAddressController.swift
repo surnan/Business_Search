264f88a560b3e6f6d10ebfab2612f1c7a8916e88
Suresh N <forsuresh@yandex.com>
2019-09-26 12:45:22 -0400

    Textview grows and shrinks appropriately OR it scrolls at maximumHeight.  Unable to perform both activities

--- Business_Search/Business_Search/ViewControllers/SearchByAddress/SearchByAddressController.swift
+++ Business_Search/Business_Search/ViewControllers/SearchByAddress/SearchByAddressController.swift
@@ -23,7 +23,7 @@ class SearchByAddressController: UIViewController, UITextViewDelegate, UITextFie
     lazy var locationTextField  = viewObject.locationTextField
     lazy var findLocationButton = viewObject.findLocationButton
     
-
+    
     
     var barButtonState = ButtonState.disabled
     
@@ -48,6 +48,7 @@ class SearchByAddressController: UIViewController, UITextViewDelegate, UITextFie
     var myTextView: UITextView = {
         let textView = UITextView()
         textView.text = "Placeholder..."
+        textView.font = UIFont(name: "Georgie", size: 25)
         textView.textColor = .lightGray
         textView.sizeToFit()
         textView.isScrollEnabled = false
@@ -60,21 +61,58 @@ class SearchByAddressController: UIViewController, UITextViewDelegate, UITextFie
     }
 }
 
+
+
+
 extension SearchByAddressController {
+    
     func textViewDidChange(_ textView: UITextView) {
-        if textView.contentSize.height >= self.textViewMaxHeight
-        {
+        let fixedWidth = myTextView.frame.size.width
+        let newSize: CGSize = myTextView.sizeThatFits(CGSize(width: fixedWidth, height: textViewMaxHeight))
+        var newFrame = myTextView.frame
+        newFrame.size = CGSize(width: newSize.width, height: newSize.height)
+        
+        if textView.contentSize.height >= self.textViewMaxHeight{
             textView.isScrollEnabled = true
-            textView.frame.size.height = textView.contentSize.height
-        }
-        else
-        {
-            textView.frame.size.height = textView.contentSize.height
+        } else {
             textView.isScrollEnabled = false
         }
     }
     
-    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.textColor == UIColor.lightGray {
+            textView.text = nil
+            textView.textColor = UIColor.black
+        }
+    }
+}
+
+
+
+
+/*
+ //Need to run on phone to see if this is necessary
+ func textViewDidEndEditing(_ textView: UITextView) {
+ textView.resignFirstResponder()
+ }
+ */
+
+//    func textViewDidChange(_ textView: UITextView) {
+//
+//        if textView.contentSize.height >= self.textViewMaxHeight
+//        {
+//            textView.isScrollEnabled = true
+//            textView.sizeToFit()
+//            //textView.frame.size.height = textView.contentSize.height
+//        }
+//        else
+//        {
+//            textView.frame.size.height = textView.contentSize.height
+//            textView.isScrollEnabled = false
+//        }
+//    }
+
+
 //    func textViewDidChangeSelection(_ textView: UITextView) {
 //        if self.view.window != nil {
 //            if textView.textColor == UIColor.lightGray {
@@ -82,12 +120,3 @@ extension SearchByAddressController {
 //            }
 //        }
 //    }
-  
-    
-    func textViewDidBeginEditing(_ textView: UITextView) {
-                if textView.textColor == UIColor.lightGray {
-                    textView.text = nil
-                    textView.textColor = UIColor.black
-                }
-    }    
-}
